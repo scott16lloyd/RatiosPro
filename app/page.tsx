568a50ad159e04bd4c14b3ffa23fc7f0899d791d
Scott Lloyd <scott16lloyd@gmail.com>
2024-01-11 15:45:53 +0000

    ui: search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { TopNavBar } from '@/components/ui/top-nav-bar';
+import { SearchBar } from '@/components/ui/search-bar';
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center gap-4">
       <TopNavBar />
+      <SearchBar />
     </main>
   );
 }
---
+++ components/ui/search-bar.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Search } from 'lucide-react';
+
+export function SearchBar() {
+  return (
+    <div className="flex w-11/12 md:w-7/12 lg:w-6/12 xl:w-4/12 items-center space-x-2 h-12">
+      <Input
+        type="email"
+        placeholder="Search with a ticker symbol e.g. APPL"
+        className="h-full w-11/12 xs:text-xs md:text-lg"
+      />
+      <Button className="bg-primary h-full w-14" type="submit" size="icon">
+        <Search size={20} />
+      </Button>
+    </div>
+  );
+}
