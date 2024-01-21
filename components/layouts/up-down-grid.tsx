cfa25b7df73c865281b7bd8e3686e6116c6004ea
Scott Lloyd <scott16lloyd@gmail.com>
2024-01-21 20:57:54 +0000

    ui: basic horizontal box ui

--- components/layouts/up-down-grid.tsx
+++ components/layouts/up-down-grid.tsx
@@ -1,9 +1,22 @@
-import { Link, ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
+'use client';
+
+import react, { useState } from 'react';
+import { Link } from 'lucide-react';
 import { Button } from '../ui/button';
-import { SmallBentoBox } from '../ui/small-bento-box';
 import { HorizontalBentoBox } from '../ui/horizontal-bento-box';
 
 export function UpDownGrid({ title = 'No title' }: { title: string }) {
+  const [activeButton, setActiveButton] = useState('popular');
+
+  const changeColor = (buttonName: string) => {
+    setActiveButton(buttonName);
+  };
+
+  const getButtonClass = (buttonName: string) => {
+    return activeButton === buttonName
+      ? 'dark:bg-white hover:dark:bg-white text-black dark:hover:text-black'
+      : '';
+  };
   return (
     <div className="group relative flex items-center justify-center p-4 gap-2 w-full h-min-content px-0">
       <div className="w-full relative z-0 px-4 items-center">
@@ -15,18 +28,44 @@ export function UpDownGrid({ title = 'No title' }: { title: string }) {
             View all
           </Link>
         </div>
-        <div className="w-full items-center gap-4 flex justify-center pb-4">
-          <Button variant="outline">Popular</Button>
-          <Button variant="outline">Tech</Button>
-          <Button variant="outline">Healthcare</Button>
+        <div className="grid grid-rows-1 grid-flow-col overflow-x-auto snap-mandatory scrollbar-hide w-full lg:px-28 2xl:px-64 md:gap-6 lg:gap-8 xl:px-46 xl:gap-10 items-content-center gap-4 pb-4">
+          <Button
+            variant="outline"
+            onClick={() => changeColor('popular')}
+            className={getButtonClass('popular')}
+          >
+            Popular
+          </Button>
+          <Button
+            variant="outline"
+            className={getButtonClass('tech')}
+            onClick={() => changeColor('tech')}
+          >
+            Tech
+          </Button>
+          <Button
+            variant="outline"
+            onClick={() => changeColor('healthcare')}
+            className={getButtonClass('healthcare')}
+          >
+            Healthcare
+          </Button>
+          <Button
+            variant="outline"
+            onClick={() => changeColor('finance')}
+            className={getButtonClass('finance')}
+          >
+            Finance
+          </Button>
+          <Button
+            variant="outline"
+            onClick={() => changeColor('energy')}
+            className={getButtonClass('energy')}
+          >
+            Energy
+          </Button>
         </div>
-        <div
-          //   ref={UseScrollContainer}
-          //   onMouseDown={handleMouseDown}
-          //   onMouseUp={handleMouseUp}
-          //   onMouseMove={handleMouseMove}
-          className="grid grid-flow-row gap-4 sm:gap-6 md:gap-7 xl:gap-12 md:px-10 lg:px-14 xl:px-18 2xl:px-28 snap-mandatory scrollbar-hide items-center w-full"
-        >
+        <div className="grid grid-flow-row gap-4 sm:gap-6 md:gap-7 xl:gap-12 md:px-10 lg:px-14 xl:px-18 2xl:px-28 snap-mandatory scrollbar-hide items-center w-full">
           <HorizontalBentoBox />
           <HorizontalBentoBox />
           <HorizontalBentoBox />
