21a671f27d7d2e44085722e67351ac0babec196e
Scott Lloyd <scott16lloyd@gmail.com>
2024-02-06 16:55:07 +0000

    fix: exposed env variable

--- hooks/index.js
+++ hooks/index.js
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 const fetchBiggestGainers = async () => {
   // Provides a list of the stocks that have gained the most value on a given day
   const response = await fetch(
-    `https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=1b230136e8ddd66509145f0916c72ac6`
+    `https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=${process.env.NEXT_PUBLIC_API_KEY}`
   );
   const data = await response.json();
   return data;
@@ -12,7 +12,7 @@ const fetchBiggestGainers = async () => {
 const fetchMostPopular = async () => {
   // Provides a list of the stocks that have the highest trading volume on a given day
   const response = await fetch(
-    `https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=1b230136e8ddd66509145f0916c72ac6`
+    `https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=${process.env.NEXT_PUBLIC_API_KEY}`
   );
   const data = await response.json();
   return data;
