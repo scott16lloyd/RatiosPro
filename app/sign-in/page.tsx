870bd816126d7ae10e647963eb2a512048200022
Scott Lloyd <scott16lloyd@gmail.com>
2024-03-08 17:06:02 +0000

    wip: login form

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -6,13 +6,14 @@ import googleIcon from '@/public/googleIcon.png';
 import { Separator } from '@/components/ui/separator';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import Link from 'next/link';
 
 export default function SignInPage() {
   return (
-    <div className="h-screen w-screen py-4 px-3">
+    <div className="h-screen w-full py-4 px-3">
       <Card className="h-full w-full ring-zinc-700 ring-1">
-        <CardContent className="p-4 h-full flex flex-col space-y-4">
-          <div className="flex flex-col gap-5 flex-grow-1">
+        <CardContent className="p-4 h-full w-full flex flex-col space-y-4">
+          <div className="flex flex-col w-full gap-5 flex-grow-1">
             <div className="flex flex-col w-max text-left">
               <span className="text-3xl font-light">Welcome back</span>
               <span className="text-md font-light text-zinc-500">
@@ -59,7 +60,7 @@ export default function SignInPage() {
               <Separator className="w-1/2" />
             </div>
           </div>
-          <div className="flex flex-col flex-grow-2 gap-4">
+          <div className="flex flex-col flex-grow-2 gap-4 w-full">
             <div className="grid w-full max-w-sm items-center gap-1.5">
               <Label htmlFor="email" className="text-zinc-500 font-light">
                 Email
@@ -67,8 +68,8 @@ export default function SignInPage() {
               <Input
                 type="email"
                 placeholder="Email"
-                className="rounded-sm"
-              ></Input>
+                className="rounded-sm py-5 w-full dark:border-zinc-700 dark:border-1"
+              />
             </div>
             <div className="grid w-full max-w-sm items-center gap-1.5">
               <Label htmlFor="password" className="text-zinc-500 font-light">
@@ -77,10 +78,31 @@ export default function SignInPage() {
               <Input
                 type="password"
                 placeholder="Password"
-                className="rounded-sm"
-              ></Input>
+                className="rounded-sm py-5 w-full dark:border-zinc-700 dark:border-1"
+              />
             </div>
-            <Button className="bg-white">Sign In</Button>
+            <Button className="p-5">
+              <span className="text-white">Sign In</span>
+            </Button>
+            <span className="text-zinc-500 font-light text-center text-xs">
+              Don’t have an account?{' '}
+              <Link href={'#'} className="underline">
+                Sign Up Now
+              </Link>
+            </span>
+          </div>
+          <div className="flex flex-col h-full justify-end">
+            <span className="text-zinc-500 font-light text-center text-xs">
+              By continuing, you agree to Ratio Pro’s{' '}
+              <Link href={'#'} className="underline">
+                Terms of service
+              </Link>{' '}
+              and{' '}
+              <Link href={'#'} className="underline">
+                Privacy Policy
+              </Link>
+              , and to receive periodic emails with updates.
+            </span>
           </div>
         </CardContent>
       </Card>
