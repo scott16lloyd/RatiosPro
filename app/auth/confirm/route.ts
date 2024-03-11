d0ced8806e628df873bbf492412ed4c189f97548
Scott Lloyd <scott16lloyd@gmail.com>
2024-03-11 20:39:17 +0000

    fix: route.ts error

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,33 +1,38 @@
-// import { type EmailOtpType } from '@supabase/supabase-js';
-// import { type NextRequest, NextResponse } from 'next/server';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { EmailOtpType } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
 
-// import { createClient } from '@/utils/supabase/supabaseClient';
+export const createServerClient = () => {
+  const cookieStore = cookies();
+  return createServerComponentClient({ cookies: () => cookieStore });
+};
 
-// export async function GET(request: NextRequest) {
-//   const { searchParams } = new URL(request.url);
-//   const token_hash = searchParams.get('token_hash');
-//   const type = searchParams.get('type') as EmailOtpType | null;
-//   const next = searchParams.get('next') ?? '/';
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const token_hash = searchParams.get('token_hash');
+  const type = searchParams.get('type') as EmailOtpType | null;
+  const next = searchParams.get('next') ?? '/';
 
-//   const redirectTo = request.nextUrl.clone();
-//   redirectTo.pathname = next;
-//   redirectTo.searchParams.delete('token_hash');
-//   redirectTo.searchParams.delete('type');
+  const redirectTo = request.nextUrl.clone();
+  redirectTo.pathname = next;
+  redirectTo.searchParams.delete('token_hash');
+  redirectTo.searchParams.delete('type');
 
-//   if (token_hash && type) {
-//     const supabase = createClient();
+  if (token_hash && type) {
+    const supabase = createServerClient();
 
-//     const { error } = await supabase.auth.verifyOtp({
-//       type,
-//       token_hash,
-//     });
-//     if (!error) {
-//       redirectTo.searchParams.delete('next');
-//       return NextResponse.redirect(redirectTo);
-//     }
-//   }
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    });
+    if (!error) {
+      redirectTo.searchParams.delete('next');
+      return NextResponse.redirect(redirectTo);
+    }
+  }
 
-//   // return the user to an error page with some instructions
-//   redirectTo.pathname = '/error';
-//   return NextResponse.redirect(redirectTo);
-// }
+  // return the user to an error page with some instructions
+  redirectTo.pathname = '/error';
+  return NextResponse.redirect(redirectTo);
+}
