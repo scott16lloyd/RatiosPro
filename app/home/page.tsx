b3449b48ab96478e5e3181b809159034210a67ba
Scott Lloyd <scott16lloyd@gmail.com>
2024-03-22 21:07:04 +0000

    fix: button nav logic and search input lag

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,10 +4,8 @@ import { TopNavBar } from '@/components/ui/top-nav-bar';
 import { SearchBar } from '@/components/ui/search-bar';
 import { LeftToRightGrid } from '@/components/layouts/left-to-right-grid';
 import { UpDownGrid } from '@/components/layouts/up-down-grid';
-import { getuser } from '@/utils/supabase/dbFunctions';
 import { redirect } from 'next/navigation';
 import { createClient } from '@/utils/supabase/supabaseClient';
-import { DataContextProvider } from '@/components/ContextProvider';
 
 export default async function Home() {
   const supabase = createClient();
