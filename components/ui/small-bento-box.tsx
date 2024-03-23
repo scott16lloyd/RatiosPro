bd8c8eaa409f5dd22a8035d22c266ecd5e1c25a9
Scott Lloyd <scott16lloyd@gmail.com>
2024-03-23 14:14:16 +0000

    wip: added heart button UI and state logic

--- components/ui/small-bento-box.tsx
+++ components/ui/small-bento-box.tsx
@@ -1,6 +1,8 @@
 import { Card, CardContent } from '@/components/ui/card';
 import Link from 'next/link';
 import { ArrowDownRight, ArrowUpRight } from 'lucide-react';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import { useState } from 'react';
 
 interface SmallBentoBoxProps {
   symbol: string;
@@ -15,14 +17,32 @@ export function SmallBentoBox({
   price,
   changesPercentage,
 }: SmallBentoBoxProps) {
+  const [isHeartFilled, setIsHeartFilled] = useState(false);
+
+  const handleHeartClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsHeartFilled(!isHeartFilled);
+  };
   return (
     <Link href={{ pathname: `/details/${symbol}`, query: { name } }}>
       <Card className="sm:w-32 md:w-52 sm:h-24 md:h-36 lg:w-72 lg:h-44 xl:w-80 xl:h-48 border-none p-2 md:p-4 noselect ring-zinc-700 ring-1">
         <CardContent className="p-0 h-full flex flex-col justify-evenly lg:gap-1">
-          <div className="flex flex-row w-full items-start">
+          <div className="flex flex-row w-full items-center">
             <span className="w-full sm:text-xxs md:text-sm lg:text-lg text-zinc-600 truncate">
               {name}
             </span>
+            {isHeartFilled ? (
+              <FaHeart
+                className="w-4 h-4 md:w-6 md:h-6 lg:w-7 lg:h-7"
+                onClick={handleHeartClick}
+              />
+            ) : (
+              <FaRegHeart
+                className="w-4 h-4 md:w-6 md:h-6 lg:w-7 lg:h-7"
+                onClick={handleHeartClick}
+              />
+            )}
           </div>
           <div className="flex items-center content-start flex-row gap-2 md:gap-4 lg:gap-6">
             <span className="text-lg md:text-3xl lg:text-4xl">{symbol}</span>
