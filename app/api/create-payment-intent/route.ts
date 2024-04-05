41c3e9d956ab5d847987ae2b6d05b80655099470
Scott Lloyd <scott16lloyd@gmail.com>
2024-04-05 11:07:19 +0100

    wip: switch to payment components

---
+++ app/api/create-payment-intent/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse, NextRequest } from 'next/server';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  typescript: true,
+});
+
+export async function POST(req: NextRequest) {
+  const { data } = await req.json();
+  const { amount } = data;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Number(amount) * 100,
+      currency: 'EUR',
+    });
+
+    return new NextResponse(paymentIntent.client_secret, { status: 200 });
+  } catch (error: any) {
+    return new NextResponse(error, {
+      status: 400,
+    });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 'use client';
 
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import PaymentForm from '@/components/ui/PaymentsForm';
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
+
 export default function LandingPage() {
   return (
     <>
       <h1>Landing Page</h1>
+      <Elements stripe={stripePromise}>
+        <PaymentForm />
+      </Elements>
     </>
   );
 }
