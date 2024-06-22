fca547ceb9ba221d6b791e7dc9cb8fab4355ac8b
Scott Lloyd <scott16lloyd@gmail.com>
2024-06-22 12:48:14 +0100

    fix: social media preview image metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,20 +9,21 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'RatiosPro | Financial Ratio Calculator',
-  description: 'A simple UI for calculating financial ratios.',
+  description:
+    'Ratios Pro is a powerful web application designed to assist investors and financial professionals in analyzing stocks by providing essential financial ratios.',
   openGraph: {
     title: 'RatiosPro | Financial Ratio Calculator',
-    description: 'A simple UI for calculating financial ratios.',
+    description:
+      'Ratios Pro is a powerful web application designed to assist investors and financial professionals in analyzing stocks by providing essential financial ratios.',
     url: 'ratiospro.com',
     images: [
       {
         url: '/images/ratios-pro-desktop.png',
-        width: 1260,
-        height: 800,
       },
     ],
     locale: 'en_EN',
     siteName: 'RatiosPro',
+    type: 'website',
   },
 };
 
