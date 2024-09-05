a8a120880d3b3bcc1fa5dd05f4e014df18714d91
Scott Lloyd <scott16lloyd@gmail.com>
2024-09-05 15:07:53 +0100

    feat: username characters for profile pic

--- components/ui/profilePicture.tsx
+++ components/ui/profilePicture.tsx
@@ -1,10 +1,22 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
-export function ProfilePicture() {
+export function ProfilePicture({ username }: { username: string }) {
+  let initials = '';
+  // Get the first two letters of the username
+  if (username && username.includes(' ')) {
+    initials = username
+      .split(' ')
+      .map((name) => name[0])
+      .join('')
+      .toUpperCase();
+  } else {
+    initials = username[0]?.toUpperCase();
+  }
+
   return (
-    <Avatar className="h-fit max-w-fit">
-      <AvatarImage src="https://github.com/shadcn.png" />
-      <AvatarFallback>CN</AvatarFallback>
+    <Avatar className="min-w-10">
+      <AvatarImage />
+      <AvatarFallback className="dark:bg-secondary">{initials}</AvatarFallback>
     </Avatar>
   );
 }
